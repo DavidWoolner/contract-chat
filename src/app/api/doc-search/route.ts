22c2c8f98f294a9628befe061229cca3f4c12d0d
Dave Woolner <davidwoolner@gmail.com>
2023-11-02 15:17:22 -0400

    Save working query

--- src/app/api/doc-search/route.ts
+++ src/app/api/doc-search/route.ts
@@ -1,11 +1,14 @@
-import { NextResponse } from 'next/server';
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { VectorDBQAChain } from "langchain/chains";
-import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { NextRequest, NextResponse } from 'next/server';
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
+import { ChatOpenAI } from 'langchain/chat_models/openai';
+import { VectorDBQAChain } from 'langchain/chains';
+import { PineconeStore } from 'langchain/vectorstores/pinecone';
 import { getPineconeIndex } from '@/app/utils/pinecone';
 
-export async function GET() {
+export async function POST(req: NextRequest) {
+  const query =
+    (await req.json()) || 'Ask me what I would like to know about the contract';
+
   const pineconeIndex = await getPineconeIndex();
 
   const vectorStore = await PineconeStore.fromExistingIndex(
@@ -13,16 +16,18 @@ export async function GET() {
     { pineconeIndex }
   );
 
+  const template =
+    'You are being asked questions by laymen regarding labor contracts for the Interntaionl Alliance of Theatrical Stage Engineers or IATSE, specifically Local 52. You job is to provide friendly, clear response. It should be succinct, but also give enough detail to help inspire further questioning. Here is your question: ';
+
   try {
-    const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" });
+    const model = new ChatOpenAI({ modelName: 'gpt-3.5-turbo' });
     const chain = VectorDBQAChain.fromLLM(model, vectorStore);
     const response = await chain.call({
-      query: "What is the 30 mile zone?"
-    })
+      query: template + query,
+    });
 
-    return NextResponse.json(response)
+    return NextResponse.json(response);
   } catch (e) {
     console.log('Error:', e);
-
   }
 }
--- src/app/api/embed-doc/route.ts
+++ src/app/api/embed-doc/route.ts
@@ -1,46 +1,19 @@
 import { getPineconeIndex } from '@/app/utils/pinecone';
-// import { PineconeClient } from '@pinecone-database/pinecone';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
-// import * as dotenv from 'dotenv';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
 import { NextResponse } from 'next/server';
 import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 
-// dotenv.config();
-
-// const client = new PineconeClient();
-
-// const setupPinecone = async () => {
-//   await client.init({
-//     apiKey: process.env.PINECONE_API_KEY as string,
-//     environment: process.env.PINECONE_ENVIRONMENT as string,
-//   });
-//   const pineconeIndex = client.Index(process.env.PINECONE_INDEX as string);
-//   return pineconeIndex;
-// };
-
-// const pineconeIndex = await setupPinecone();
-
 const loader = new PDFLoader(
-  'src/app/docs/Local 52 Majors 2018 Searchable.pdf',
+  // 'src/app/docs/Local 52 Majors 2018 Searchable.pdf',
+  'src/app/docs/Deal Memorandum Individual -- David Woolner.pdf',
+
   { splitPages: false }
 );
 
-// const getDocs = async (pineconeIndex) => {
-//   const docs = await loader.load();
-//   // await PineconeStore.fromDocuments(docs, new OpenAIEmbeddings(), {
-//   //   pineconeIndex,
-//   // });
-// }
-
 export async function GET() {
   try {
-    // await client.init({
-    //   apiKey: process.env.PINECONE_API_KEY as string,
-    //   environment: process.env.PINECONE_ENVIRONMENT as string,
-    // });
-    // const pineconeIndex = client.Index(process.env.PINECONE_INDEX as string);
     const pineconeIndex = await getPineconeIndex();
 
     const docs = await loader.load();
